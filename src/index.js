16afc0477255687c3a7c3d02fa3f53c98c5c8a89
Loly Maluf <lolymaluf@gmail.com>
2022-09-01 23:59:00 -0300

    Requisito 02

--- src/index.js
+++ src/index.js
@@ -18,6 +18,15 @@ app.listen(PORT, () => {
 });
 
 app.get('/talker', async (_req, res) => {
-  const resp = await getAllTalkers();
-  res.status(200).json(resp);
+  const respList = await getAllTalkers();
+  res.status(200).json(respList);
 });
+
+app.get('/talker/:id', async (req, res) => {
+  const respList = await getAllTalkers();
+  const talkerById = respList.find((talker) => talker.id === Number(req.params.id));
+  if (talkerById) {
+    return res.status(200).json(talkerById);
+  }
+  return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+})
